42bce2e0d61fd2abbfcb29d869ee95754ebac5ab
Maxwell <xiaohui.zhang0@aliyun.com>
2020-10-15 09:10:45 +0800

    1、集成和风天气模块 2、界面优化

--- client/app.js
+++ client/app.js
@@ -17,5 +17,30 @@ App({
     onLaunch: function () {
         Auth.checkSession();
     },
-    
+    onLaunch () {
+        wx.cloud.init({
+          env: 'envid',
+          traceUser: true,
+        })
+        wx.getSystemInfo({
+          success: (res) => {
+            this.globalData.systeminfo = res
+            this.globalData.isIPhoneX = /iphonex/gi.test(res.model.replace(/\s+/, ''))
+          },
+        })
+      },
+      globalData: {
+        // 是否保持常亮，离开小程序失效
+        keepscreenon:false,
+        systeminfo: {},
+        isIPhoneX: false,
+        key: '',//这里替换成个人和风天气注册应用的key   https://console.heweather.com
+        weatherIconUrl: 'https://cdn.heweather.com/cond_icon/',
+        requestUrl: {
+          weather: 'https://free-api.heweather.com/s6/weather',
+          hourly: 'https://free-api.heweather.com/s6/weather/hourly',
+        },
+      },
 })
+
+
